7e3d605a6e8408f0fd9dc85cd7cb27055c9d1a34
Batuhan Duras <batugane@gmail.com>
2025-07-21 16:13:11 +0300

    Initial commit of PadPlay project, including main app structure, UI components, asset catalogs, and test files. Added ContentView, PadPlayApp, entitlements, and initial test cases for unit and UI testing.

---
+++ PadPlay/PadPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadPlayApp.swift
+//  PadPlay
+//
+//  Created by Batuhan Duras on 21.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct PadPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PadPlayTests/PadPlayTests.swift
@@ -0,0 +1,17 @@
+//
+//  PadPlayTests.swift
+//  PadPlayTests
+//
+//  Created by Batuhan Duras on 21.07.2025.
+//
+
+import Testing
+@testable import PadPlay
+
+struct PadPlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
